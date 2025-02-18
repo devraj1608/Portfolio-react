3541f65e563a0c1239319ec7294fbddd3944466c
devraj1608 <devrajsaini2005@gmail.com>
2025-02-18 15:44:03 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], 
-  base:"/Portfolio-react"
+  plugins: [react()],
+  base:"/Portfolio-react",
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1000 kB
+  }
 });
